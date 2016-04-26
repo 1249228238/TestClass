ea6f9bcd426d6069807bcd07d8709c8c187271a8
1249228238 <1249228238@qq.com>
2016-04-26 19:46:52 +0800

    first

---
+++ src/testclass/TestClass.java
@@ -0,0 +1,13 @@
+package testclass;
+
+public class TestClass {
+	public static void main(String[] args) {
+		int Score=0;
+		if (Score>50) {
+			System.out.println("¼°¸ñ");
+			
+		}
+		System.out.println();
+	}
+
+}
